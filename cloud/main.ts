aac334b95236050b6d06859dbdc30f5a46326f70
Emmanuel Francis <69248635+xiangdee@users.noreply.github.com>
2023-01-04 10:59:30 +0100

    renamed cloud run main.js to main.ts

---
+++ cloud/main.ts
@@ -0,0 +1,26 @@
+declare const Parse: any;
+import './generated/evmApi';
+import './generated/solApi';
+import { requestMessage } from '../auth/authService';
+
+Parse.Cloud.define('requestMessage', async ({ params }: any) => {
+  const { address, chain, network } = params;
+
+  const message = await requestMessage({
+    address,
+    chain,
+    network,
+  });
+
+  return { message };
+});
+
+Parse.Cloud.define('getPluginSpecs', () => {
+  // Not implemented, only excists to remove client-side errors when using the moralis-v1 package
+  return [];
+});
+
+Parse.Cloud.define('getServerTime', () => {
+  // Not implemented, only excists to remove client-side errors when using the moralis-v1 package
+  return null;
+});
